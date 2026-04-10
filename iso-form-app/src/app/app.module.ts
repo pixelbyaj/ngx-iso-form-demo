f682e7a867f86f517296c8661413d08913301284
abhishek-netlink <abhishek@netlink-testlabs.com>
2026-04-10 17:25:44 +0800

    update

--- iso-form-app/src/app/app.module.ts
+++ iso-form-app/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { MatInputModule } from '@angular/material/input';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
@@ -32,6 +33,7 @@ export function HttpLoaderFactory(http: HttpClient) {
     AppRoutingModule,
     MatStepperModule,
     MatButtonModule,
+    MatCardModule,
     FormsModule,
     ReactiveFormsModule,
     MatFormFieldModule,
